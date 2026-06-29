37be022b6bbc9a55ebe532543675d9aee815638b
Muggshotz <muggshotzdesigns@gmail.com>
2026-06-29 19:54:24 -0400

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -24,6 +24,6 @@ export default async function handler(req, res) {
 
     return res.status(200).json({ url: session.url });
   } catch (error) {
-    return res.status(500).json({ error: error.message });
+    return res.status(500).json({ error: error.message, type: error.type || "unknown" });
   }
 }
